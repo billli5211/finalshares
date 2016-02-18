18bcc84f36b07f12449981e9a6e9fc1ffcc36849
bill <shilongdred2163.com>
2016-02-18 19:39:05 +0800

    re-org project struction

---
+++ iosapp/Main/Plugin/Shop/ShopCar/shopConfig.h
@@ -0,0 +1,17 @@
+//
+//  shopConfig.h
+//  iosapp
+//
+//  Created by Bill Li on 2/18/16.
+//  Copyright © 2016 oschina. All rights reserved.
+//
+
+#ifndef shopConfig_h
+#define shopConfig_h
+
+#define SCREEN_WIDTH [UIScreen mainScreen].bounds.size.width
+#define SCREEN_HEIGHT [UIScreen mainScreen].bounds.size.height
+#define Width [UIScreen mainScreen].bounds.size.width/320.0
+#define Height [UIScreen mainScreen].bounds.size.height/568.0
+
+#endif /* shopConfig_h */
